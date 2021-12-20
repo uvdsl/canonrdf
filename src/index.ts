a6653517ce5c982812dce06a516038908ed59eb1
ik1533 <braun@kit.edu>
2021-12-20 14:19:53 +0100

    exported Algo 3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { hashBNodes } from './Algorithm_1_Deterministically_hashing_blank_nodes/hashingBNs';
 export { hashBNodesPerSplit } from './Algorithm_2_Hashing_blank_nodes_splits_independently/hashingBNsplits';
+export { isoCanonicalise } from './Algorithm_3_Computing_an_iso-canonical_version_of_an_RDF_graph/isoCanonicalising';
